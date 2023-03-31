06f1d9d03367a33ab34798abe20e8f6dae1e649a
Gelik <5526052@gmail.com>
2023-03-31 19:42:58 +0300

    Result

---
+++ js/script.js
@@ -0,0 +1,30 @@
+
+//2//
+let a1 = 5 % 3, a2 = 3 % 5, a3 = 5 + '3', a4 = '5' - 3, a5 = 75 + 'кг', a6 = 785 * 653, a7 = 100 / 25, a8 = 0*0, a9 = 0 / 2, a10 = 89 / 0, a11 = 98 + 2, a12 = 5 - 98, a13 = (8 + 56 * 4) / 5, a14 = (9 - 12) * 7 / (5 + 2), a15 = +"123", a16 = 1 || 0, a17 = false || true, a18 = true > 0;
+console.log(typeof a1, typeof a2, typeof a3, typeof a4, typeof a5, typeof a6, typeof a7, typeof a8, typeof a9, typeof a10, typeof a11, typeof a12, typeof a13, typeof a14, typeof a15, typeof a16, typeof a17, typeof a18);
+//3//
+let height = 23;
+let width = 10;
+let spryam = height * width;
+console.log(spryam);
+//4//
+let height_cilindr = 10;
+let vcilindra = 3.14*(a7*a7)*height_cilindr;
+console.log(vcilindra);
+//5//
+let r = 5;
+let skruga = 3.14*(r*r);
+console.log(skruga);
+//6//
+let a = 5;
+let b = 7;
+let h = 10;
+let strap = h/2*(a+b);
+console.log(strap);
+//7//
+let s = 2000000;
+let p = 1.1;
+let years = 5;
+let pereplata = (s * p - s) * years;
+console.log(pereplata);
+//8 К сожалению не смог решить данную задачу, так как все Варианты решения которые я нашел в сети требуют более глубоких знаний в JS Есть еще Вариант что я непонял саму постановку задачи))//
